816164d1c763e07d2e2ef5dc333f875dca95a778
depsterr <dragonvale09@gmail.com>
2019-11-04 13:35:34 +0100

    fixed login AGAIN, I should really check if it's working before I commit

--- files/.scripts/login.sh
+++ files/.scripts/login.sh
@@ -1,7 +1,7 @@
 # startx if logging in in the startup tty
 if [ "$(tty)" = "/dev/tty1" ]; then
 	clear
-	echo -e "Welcome!\n\nWhat do you want to open?\n\n[x] start the x server\n[t]continue to tty\n[T] open tmux in the tty\n\n"
+	echo "Welcome!\n\nWhat do you want to open?\n\n[x] start the x server\n[t]continue to tty\n[T] open tmux in the tty\n\n"
 	read xtT
 	case $xtT in
 		[x]* ) startx;;
