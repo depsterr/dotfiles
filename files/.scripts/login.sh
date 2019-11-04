e63501b8d3bb52c09d26aece1e02ee72adf5df71
depsterr <dragonvale09@gmail.com>
2019-11-04 13:48:28 +0100

    fixed it so that login only occurs once in the same tty

--- files/.scripts/login.sh
+++ files/.scripts/login.sh
@@ -1,5 +1,6 @@
 # startx if logging in in the startup tty
-if [ "$(tty)" = "/dev/tty1" ]; then
+if [ "$(tty)" = "/dev/tty1" && "$loggedin" == "" ]; then
+	export loggedin = true
 	clear
 	echo "Welcome!\n\nWhat do you want to open?\n\n[x] start the x server\n[t] continue to tty\n[T] open tmux in the tty\n\n"
 	read xtT
