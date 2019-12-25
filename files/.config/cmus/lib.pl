6b78c97eb52bfed8b31b86f93502009fe4a3c481
depsterr <dragonvale09@gmail.com>
2019-12-25 13:14:37 +0100

    updated dotfiles

--- files/.config/cmus/lib.pl
+++ files/.config/cmus/lib.pl
@@ -1,11 +1,29 @@
-/home/deppy/Music/macintosh-plus/floral-shoppe/B5 - 外ギン Aviation.flac
-/home/deppy/Music/macintosh-plus/floral-shoppe/A1 - ブート.flac
-/home/deppy/Music/macintosh-plus/floral-shoppe/A2 - リサフランク420 現代のコンピュー.flac
-/home/deppy/Music/macintosh-plus/floral-shoppe/A3 - 花の専門店.flac
-/home/deppy/Music/macintosh-plus/floral-shoppe/A4 - ライブラリ.flac
-/home/deppy/Music/macintosh-plus/floral-shoppe/A5 - 地理.flac
-/home/deppy/Music/macintosh-plus/floral-shoppe/B1 - Eccoと悪寒ダイビング.flac
-/home/deppy/Music/macintosh-plus/floral-shoppe/B2 - 数学.flac
-/home/deppy/Music/macintosh-plus/floral-shoppe/B3 - 待機.flac
-/home/deppy/Music/macintosh-plus/floral-shoppe/B4 - ピコ.flac
-/home/deppy/Music/macintosh-plus/floral-shoppe/B6 - て.flac
+/home/deppy/Music/dan-mason/hypnagogia/Dan Mason - Hypnagogia - 01 Insomnia.flac
+/home/deppy/Music/dan-mason/hypnagogia/Dan Mason - Hypnagogia - 02 Melatonin High.flac
+/home/deppy/Music/dan-mason/hypnagogia/Dan Mason - Hypnagogia - 03 Shade.flac
+/home/deppy/Music/dan-mason/hypnagogia/Dan Mason - Hypnagogia - 04 Fade.flac
+/home/deppy/Music/dan-mason/hypnagogia/Dan Mason - Hypnagogia - 05 Go Away.flac
+/home/deppy/Music/dan-mason/hypnagogia/Dan Mason - Hypnagogia - 06 Vision.flac
+/home/deppy/Music/dan-mason/hypnagogia/Dan Mason - Hypnagogia - 07 Stop Me.flac
+/home/deppy/Music/dan-mason/hypnagogia/Dan Mason - Hypnagogia - 08 Good Night.flac
+/home/deppy/Music/dan-mason/hypnagogia/Dan Mason - Hypnagogia - 09 Hopefully Forever.flac
+/home/deppy/Music/dan-mason/miami-virtual-2.0/Dan Mason ダン·メイソン - Miami Virtual 2.0 - 01 お帰りなさい​!​!.flac
+/home/deppy/Music/dan-mason/miami-virtual-2.0/Dan Mason ダン·メイソン - Miami Virtual 2.0 - 02 Don't Be フールド.flac
+/home/deppy/Music/dan-mason/miami-virtual-2.0/Dan Mason ダン·メイソン - Miami Virtual 2.0 - 03 My 恋人.flac
+/home/deppy/Music/dan-mason/miami-virtual-2.0/Dan Mason ダン·メイソン - Miami Virtual 2.0 - 04 So Sexy.flac
+/home/deppy/Music/dan-mason/miami-virtual-2.0/Dan Mason ダン·メイソン - Miami Virtual 2.0 - 05 After Sunset.flac
+/home/deppy/Music/dan-mason/miami-virtual-2.0/Dan Mason ダン·メイソン - Miami Virtual 2.0 - 06 Deep Sea ダイビング.flac
+/home/deppy/Music/dan-mason/miami-virtual-2.0/Dan Mason ダン·メイソン - Miami Virtual 2.0 - 07 Night Drive.flac
+/home/deppy/Music/dan-mason/miami-virtual-2.0/Dan Mason ダン·メイソン - Miami Virtual 2.0 - 08 7 to 5.flac
+/home/deppy/Music/dan-mason/miami-virtual-2.0/Dan Mason ダン·メイソン - Miami Virtual 2.0 - 09 Miami 夜.flac
+/home/deppy/Music/dan-mason/miami-virtual-2.0/Dan Mason ダン·メイソン - Miami Virtual 2.0 - 10 Sayonara Sunshine.flac
+/home/deppy/Music/dan-mason/miami-virtual-2.0/Dan Mason ダン·メイソン - Miami Virtual 2.0 - 11 X Gonna Funk Ya.flac
+/home/deppy/Music/dan-mason/summer-love/Dan Mason ダン·メイソン - Summer Love - 01 Hold Me Close.flac
+/home/deppy/Music/dan-mason/summer-love/Dan Mason ダン·メイソン - Summer Love - 02 Summer Melody.flac
+/home/deppy/Music/dan-mason/summer-love/Dan Mason ダン·メイソン - Summer Love - 03 Beach Body.flac
+/home/deppy/Music/dan-mason/summer-love/Dan Mason ダン·メイソン - Summer Love - 04 Na Na.flac
+/home/deppy/Music/dan-mason/summer-love/Dan Mason ダン·メイソン - Summer Love - 05 Love at First Sight.flac
+/home/deppy/Music/dan-mason/summer-love/Dan Mason ダン·メイソン - Summer Love - 06 Splash​!​!.flac
+/home/deppy/Music/dan-mason/summer-love/Dan Mason ダン·メイソン - Summer Love - 07 Shopping Spree.flac
+/home/deppy/Music/dan-mason/summer-love/Dan Mason ダン·メイソン - Summer Love - 08 Natsu Ai.flac
+/home/deppy/Music/dan-mason/summer-love/Dan Mason ダン·メイソン - Summer Love - 09 I Need You Now.flac
