e59ce40c6a1579f59948ef676f97f6550e96ccc1
depsterr <dragonvale09@gmail.com>
2019-12-30 01:49:43 +0100

    added torrent and league script

--- files/.config/cmus/lib.pl
+++ files/.config/cmus/lib.pl
@@ -1,29 +1,46 @@
-/home/deppy/Music/dan-mason/hypnagogia/Dan Mason - Hypnagogia - 01 Insomnia.flac
-/home/deppy/Music/dan-mason/hypnagogia/Dan Mason - Hypnagogia - 02 Melatonin High.flac
-/home/deppy/Music/dan-mason/hypnagogia/Dan Mason - Hypnagogia - 03 Shade.flac
-/home/deppy/Music/dan-mason/hypnagogia/Dan Mason - Hypnagogia - 04 Fade.flac
-/home/deppy/Music/dan-mason/hypnagogia/Dan Mason - Hypnagogia - 05 Go Away.flac
-/home/deppy/Music/dan-mason/hypnagogia/Dan Mason - Hypnagogia - 06 Vision.flac
-/home/deppy/Music/dan-mason/hypnagogia/Dan Mason - Hypnagogia - 07 Stop Me.flac
-/home/deppy/Music/dan-mason/hypnagogia/Dan Mason - Hypnagogia - 08 Good Night.flac
-/home/deppy/Music/dan-mason/hypnagogia/Dan Mason - Hypnagogia - 09 Hopefully Forever.flac
-/home/deppy/Music/dan-mason/miami-virtual-2.0/Dan Mason ダン·メイソン - Miami Virtual 2.0 - 01 お帰りなさい​!​!.flac
-/home/deppy/Music/dan-mason/miami-virtual-2.0/Dan Mason ダン·メイソン - Miami Virtual 2.0 - 02 Don't Be フールド.flac
-/home/deppy/Music/dan-mason/miami-virtual-2.0/Dan Mason ダン·メイソン - Miami Virtual 2.0 - 03 My 恋人.flac
-/home/deppy/Music/dan-mason/miami-virtual-2.0/Dan Mason ダン·メイソン - Miami Virtual 2.0 - 04 So Sexy.flac
-/home/deppy/Music/dan-mason/miami-virtual-2.0/Dan Mason ダン·メイソン - Miami Virtual 2.0 - 05 After Sunset.flac
-/home/deppy/Music/dan-mason/miami-virtual-2.0/Dan Mason ダン·メイソン - Miami Virtual 2.0 - 06 Deep Sea ダイビング.flac
-/home/deppy/Music/dan-mason/miami-virtual-2.0/Dan Mason ダン·メイソン - Miami Virtual 2.0 - 07 Night Drive.flac
-/home/deppy/Music/dan-mason/miami-virtual-2.0/Dan Mason ダン·メイソン - Miami Virtual 2.0 - 08 7 to 5.flac
-/home/deppy/Music/dan-mason/miami-virtual-2.0/Dan Mason ダン·メイソン - Miami Virtual 2.0 - 09 Miami 夜.flac
-/home/deppy/Music/dan-mason/miami-virtual-2.0/Dan Mason ダン·メイソン - Miami Virtual 2.0 - 10 Sayonara Sunshine.flac
-/home/deppy/Music/dan-mason/miami-virtual-2.0/Dan Mason ダン·メイソン - Miami Virtual 2.0 - 11 X Gonna Funk Ya.flac
-/home/deppy/Music/dan-mason/summer-love/Dan Mason ダン·メイソン - Summer Love - 01 Hold Me Close.flac
-/home/deppy/Music/dan-mason/summer-love/Dan Mason ダン·メイソン - Summer Love - 02 Summer Melody.flac
-/home/deppy/Music/dan-mason/summer-love/Dan Mason ダン·メイソン - Summer Love - 03 Beach Body.flac
-/home/deppy/Music/dan-mason/summer-love/Dan Mason ダン·メイソン - Summer Love - 04 Na Na.flac
-/home/deppy/Music/dan-mason/summer-love/Dan Mason ダン·メイソン - Summer Love - 05 Love at First Sight.flac
-/home/deppy/Music/dan-mason/summer-love/Dan Mason ダン·メイソン - Summer Love - 06 Splash​!​!.flac
-/home/deppy/Music/dan-mason/summer-love/Dan Mason ダン·メイソン - Summer Love - 07 Shopping Spree.flac
-/home/deppy/Music/dan-mason/summer-love/Dan Mason ダン·メイソン - Summer Love - 08 Natsu Ai.flac
-/home/deppy/Music/dan-mason/summer-love/Dan Mason ダン·メイソン - Summer Love - 09 I Need You Now.flac
+/home/deppy/Music/blank-banshee/zero/Blank Banshee - Blank Banshee 0 - 01 B-Start Up.flac
+/home/deppy/Music/blank-banshee/zero/Blank Banshee - Blank Banshee 0 - 02 Wavestep.flac
+/home/deppy/Music/blank-banshee/zero/Blank Banshee - Blank Banshee 0 - 03 Bathsalts.flac
+/home/deppy/Music/blank-banshee/zero/Blank Banshee - Blank Banshee 0 - 04 Ammonia Clouds.flac
+/home/deppy/Music/blank-banshee/zero/Blank Banshee - Blank Banshee 0 - 05 Venus Death Trap.flac
+/home/deppy/Music/blank-banshee/zero/Blank Banshee - Blank Banshee 0 - 06 Hyper Object.flac
+/home/deppy/Music/blank-banshee/zero/Blank Banshee - Blank Banshee 0 - 07 Photosynthesis.flac
+/home/deppy/Music/blank-banshee/zero/Blank Banshee - Blank Banshee 0 - 08 Deep Space.flac
+/home/deppy/Music/blank-banshee/zero/Blank Banshee - Blank Banshee 0 - 09 Dreamcast.flac
+/home/deppy/Music/blank-banshee/zero/Blank Banshee - Blank Banshee 0 - 10 Cyber Zodiac.flac
+/home/deppy/Music/blank-banshee/zero/Blank Banshee - Blank Banshee 0 - 11 Teen Pregnancy.flac
+/home/deppy/Music/blank-banshee/zero/Blank Banshee - Blank Banshee 0 - 12 Purity Boys.flac
+/home/deppy/Music/blank-banshee/zero/Blank Banshee - Blank Banshee 0 - 13 Visualization.flac
+/home/deppy/Music/blank-banshee/zero/Blank Banshee - Blank Banshee 0 - 14 World Vision.flac
+/home/deppy/Music/blank-banshee/zero/Blank Banshee - Blank Banshee 0 - 15 B-Shut Down-Depression.flac
+/home/deppy/Music/blank-banshee/one/Blank Banshee - Blank Banshee 1 - 01 B-Infinite Login.flac
+/home/deppy/Music/blank-banshee/one/Blank Banshee - Blank Banshee 1 - 02 Eco Zones.flac
+/home/deppy/Music/blank-banshee/one/Blank Banshee - Blank Banshee 1 - 03 Metamaterial.flac
+/home/deppy/Music/blank-banshee/one/Blank Banshee - Blank Banshee 1 - 04 Anxiety Online !.flac
+/home/deppy/Music/blank-banshee/one/Blank Banshee - Blank Banshee 1 - 05 Java Clouds-مجسمه سازی مهتاب.flac
+/home/deppy/Music/blank-banshee/one/Blank Banshee - Blank Banshee 1 - 06 LSD Polyphony.flac
+/home/deppy/Music/blank-banshee/one/Blank Banshee - Blank Banshee 1 - 07 Conflict Minerals.flac
+/home/deppy/Music/blank-banshee/one/Blank Banshee - Blank Banshee 1 - 08 Metal Rain.flac
+/home/deppy/Music/blank-banshee/one/Blank Banshee - Blank Banshee 1 - 09 Big Gulp.flac
+/home/deppy/Music/blank-banshee/one/Blank Banshee - Blank Banshee 1 - 10 Doldrum Corp..flac
+/home/deppy/Music/blank-banshee/one/Blank Banshee - Blank Banshee 1 - 11 Cyber Slums.flac
+/home/deppy/Music/blank-banshee/one/Blank Banshee - Blank Banshee 1 - 12 Realization.flac
+/home/deppy/Music/blank-banshee/one/Blank Banshee - Blank Banshee 1 - 13 Solar Plexus.flac
+/home/deppy/Music/blank-banshee/one/Blank Banshee - Blank Banshee 1 - 14 Paradise Disc -芸能山城組.flac
+/home/deppy/Music/blank-banshee/one/Blank Banshee - Blank Banshee 1 - 15 B-Hidden-Reality.flac
+/home/deppy/Music/blank-banshee/mega/Blank Banshee - MEGA - 01 BIOS.flac
+/home/deppy/Music/blank-banshee/mega/Blank Banshee - MEGA - 02 My Machine.flac
+/home/deppy/Music/blank-banshee/mega/Blank Banshee - MEGA - 03 Frozen Flame.flac
+/home/deppy/Music/blank-banshee/mega/Blank Banshee - MEGA - 04 Gunshots.flac
+/home/deppy/Music/blank-banshee/mega/Blank Banshee - MEGA - 05 Megaflora.flac
+/home/deppy/Music/blank-banshee/mega/Blank Banshee - MEGA - 06 Ecco Chamber.flac
+/home/deppy/Music/blank-banshee/mega/Blank Banshee - MEGA - 07 Holograffiti.flac
+/home/deppy/Music/blank-banshee/mega/Blank Banshee - MEGA - 08 XENOS.flac
+/home/deppy/Music/blank-banshee/mega/Blank Banshee - MEGA - 09 Sandclock.flac
+/home/deppy/Music/blank-banshee/mega/Blank Banshee - MEGA - 10 Hungry Ghost.flac
+/home/deppy/Music/blank-banshee/mega/Blank Banshee - MEGA - 11 Web Ring.flac
+/home/deppy/Music/blank-banshee/mega/Blank Banshee - MEGA - 12 Meteor Blade.flac
+/home/deppy/Music/blank-banshee/mega/Blank Banshee - MEGA - 13 JUNO.flac
+/home/deppy/Music/blank-banshee/mega/Blank Banshee - MEGA - 14 Cerulean.flac
+/home/deppy/Music/blank-banshee/mega/Blank Banshee - MEGA - 15 EXOS.flac
+/home/deppy/Music/blank-banshee/metamorphasis/Blank Banshee - Metamorphosis - 01 Metamorphosis.flac
